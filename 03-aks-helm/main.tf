753d02fd9345eda3ab55c7fb84fab643ca645f9f
NKStupid <64253633+NKStupid@users.noreply.github.com>
2020-05-26 12:52:30 +0800

    Update main.tf

--- 03-aks-helm/main.tf
+++ 03-aks-helm/main.tf
@@ -15,13 +15,13 @@ provider "helm" {
   }
 }
 
-# variable "service_principal_client_id" {
-#   description = "The Client ID for the Service Principal"
-# }
+variable "service_principal_client_id" {
+  description = "The Client ID for the Service Principal"
+}
 
-# variable "service_principal_client_secret" {
-#   description = "The Client Secret for the Service Principal"
-# }
+variable "service_principal_client_secret" {
+  description = "The Client Secret for the Service Principal"
+}
 
 resource "azurerm_resource_group" "rg" {
   name     = "aks-test"
