7f16db7fdf1c9839b2feb8cd55d472b879b30e6c
NKStupid <64253633+NKStupid@users.noreply.github.com>
2020-05-27 12:19:44 +0800

    Update main.tf

--- 02-aks/main.tf
+++ 02-aks/main.tf
@@ -19,14 +19,10 @@ data "azurerm_resource_group" "example" {
   name = "aks-csi"
 }
 
-output "id" {
-  value = data.azurerm_resource_group.example.id
-}
-
 data "azurerm_subnet" "example" {
   name                 = "aks-subnet"
   virtual_network_name = "aks-vnet"
-  resource_group_name  = azurerm_resource_group.example.name
+  resource_group_name  = data.azurerm_resource_group.example.name
 }
 
 output "subnet_id" {
@@ -35,7 +31,7 @@ output "subnet_id" {
 
 resource "azurerm_kubernetes_cluster" "cluster" {
   name       = "aks"
-  location   = azurerm_resource_group.example.location
+  location   = data.azurerm_resource_group.example.location
   dns_prefix = "aks"
 
   resource_group_name = "${azurerm_resource_group.rg.name}"
@@ -46,7 +42,7 @@ resource "azurerm_kubernetes_cluster" "cluster" {
     count          = "1"
     vm_size        = "Standard_D1s_v3"
     os_type        = "Linux"
-    vnet_subnet_id = subnet_id
+    vnet_subnet_id = output.subnet_id
   }
 
   service_principal {
